3c9f7dac13b9c2e2ddfb39606467ffb67e7132d0
7erry <twal7ers@gmail.com>
2020-01-10 14:00:24 -0500

    Changed SOURCE_TOPIC_NAME & SINK_MAP_NAME to use the same name.

--- src/main/java/Kafka2IMapJson.java
+++ src/main/java/Kafka2IMapJson.java
@@ -16,21 +16,21 @@ import static java.util.concurrent.TimeUnit.NANOSECONDS;
 
 /**
  * A sample which demonstrates how to consume items Kafka topic with standard JSON
- * serialization into an IMap with same name
+ * serialization (kafka.connect) into an IMap.
  */
 public class Kafka2IMapJson {
 
     private static final String BROKER_HOST = "kafka";
     private static final String BROKER_PORT = "9092";
     private static final String AUTO_OFFSET_RESET = "earliest";
-    private static final String TOPIC = "sp500_stocks";
-    private static final String SINK_MAP_NAME = "securities_master.sp500_stocks";
+    private static final String SOURCE_TOPIC_NAME = "sp500_stocks";
+    private static final String SINK_MAP_NAME = SOURCE_TOPIC_NAME;
 
 
     private static Pipeline buildPipeline() {
         System.out.println("Starting Pipeline");
 
-	Properties properties = new Properties();
+        Properties properties = new Properties();
         properties.setProperty("group.id", "jet-cdc-demo");
         properties.setProperty("bootstrap.servers", BROKER_HOST + ':' + BROKER_PORT);
         properties.setProperty("key.deserializer", JsonDeserializer.class.getCanonicalName());
@@ -42,7 +42,7 @@ public class Kafka2IMapJson {
             HazelcastJsonValue key = new HazelcastJsonValue(record.key().toString());
             HazelcastJsonValue value = new HazelcastJsonValue(record.value().toString());
             return Util.entry(key, value);
-        }, TOPIC))
+        }, SOURCE_TOPIC_NAME))
                 .withoutTimestamps()
                 .peek()
                 .drainTo(Sinks.map(SINK_MAP_NAME));
@@ -54,17 +54,10 @@ public class Kafka2IMapJson {
     }
 
     private void go() throws InterruptedException {
-
-
             JetInstance jet = JetBootstrap.getInstance();
             //long start = System.nanoTime();
-
             Job job = jet.newJob(buildPipeline());
-
     }
-
-
-
 
     private static Properties props(String... kvs) {
         final Properties props = new Properties();
@@ -75,6 +68,3 @@ public class Kafka2IMapJson {
     }
 
 }
-
-
-
